67194108a9e06e2f5805900befda19e584a1b0ff
Chris Bistline <chris.bistline@gmail.com>
2023-05-06 13:45:07 -0500

    adds interface for Task, moves task data to db file

---
+++ src/database/Tasks.ts
@@ -0,0 +1,25 @@
+import { Task } from "../interfaces/Task";
+
+export const tasks: Task[] = [
+  {
+    id: 1,
+    name: "Task 1",
+    description: "This is task 1",
+    dueDate: "2023-05-03",
+    status: "Completed",
+  },
+  {
+    id: 2,
+    name: "Task 2",
+    description: "This is task 2",
+    dueDate: "2023-05-05",
+    status: "New",
+  },
+  {
+    id: 3,
+    name: "Task 3",
+    description: "This is task 3",
+    dueDate: "2023-05-07",
+    status: "New",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { tasks } from "./database/Tasks";
+import { Task } from "./interfaces/Task";
 
 const app = express();
 const PORT = 3000;
@@ -11,35 +13,35 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-const tasks = [
-  { id: 1, title: "Task 1", description: "This is task 1" },
-  { id: 2, title: "Task 2", description: "This is task 2" },
-  { id: 3, title: "Task 3", description: "This is task 3" },
-];
+app.get("/api/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  const task: Task = tasks.find((task) => task.id === parseInt(id));
+  res.json(task);
+});
 
 app.get("/api/tasks", (req, res) => {
-
   res.json(tasks);
 });
 
 app.post("/api/tasks", (req, res) => {
-  const task = { id: Date.now(), ...req.body };
+  const task: Task = { id: Date.now(), ...req.body };
   tasks.push(task);
   res.json(task);
 });
 
 app.put("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
-  const index = tasks.findIndex((task) => task.id === +id);
+  const index = tasks.findIndex((task) => task.id === parseInt(id));
   tasks[index] = { ...tasks[index], ...req.body };
   res.json(tasks[index]);
 });
 
 app.delete("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
-  const index = tasks.findIndex((task) => task.id === +id);
+  const index = tasks.findIndex((task) => task.id === parseInt(id));
   tasks.splice(index, 1);
-  res.json({ message: "Task deleted" });});
+  res.json({ message: "Task deleted", id });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
